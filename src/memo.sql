2b6d7e8c0df7ab2c37b0520a4947759572be1072
AltHiroaki <s2410133@wakayama-u.ac.jp>
2026-09-08 07:42:55 +0000

    テスト機能の追加

---
+++ src/memo.sql
@@ -0,0 +1,3 @@
+INSERT INTO `tabemono_list` (`user_name`, `tabemono`) VALUES ('しん', '焼肉');
+INSERT INTO `trx_comments` (`user_id`,`text`) VALUES ('27','招き猫');
+INSERT INTO `trx_comments` (`user_id`,`text`) VALUES ('26','曽爾高原');
